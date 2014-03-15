3d7caa555bbea165511b31d4a22f7f4b4cb760e8
Zaolin <zaolin@das-labor.org>
2014-03-15 04:04:26 +0100

    cleanup of bullshit

--- config.h
+++ config.h
@@ -3,12 +3,12 @@
 
 // uart.[ch] defines
 #define UART_BAUD_RATE B230400
-//#define UART_BAUD_RATE B57600
+//#define UART_BAUD_RATE B5v7600
 
 
 #ifndef __CYGWIN__
-	#define SIGINFO_HAS_SI_BAND
-	#define SHOW_UCONTEXT_INFO
+#define SIGINFO_HAS_SI_BAND
+#define SHOW_UCONTEXT_INFO
 #endif
 
 #endif
